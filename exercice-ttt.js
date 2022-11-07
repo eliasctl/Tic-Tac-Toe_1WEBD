b5de466d01bbc28f9ecd0411301261b65621631d
Dev&+ <45708451+Eliftb186@users.noreply.github.com>
2022-11-07 15:31:16 +0100

    suppr dissier ttt

--- exercice-ttt.js
+++
@@ -1,9 +0,0 @@
-const etatJeu = {
-  cellules: [
-    [null, null, null],
-    [null, null, null],
-    [null, null, null],
-  ],
-  // Deux joueurs possibles : "BLEU" ou "ORANGE"
-  joueur: 'BLEU',
-};
