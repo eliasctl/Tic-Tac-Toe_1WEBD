5246490f2f91ca05eccad68990e16f0cca910647
Dev&+ <45708451+Eliftb186@users.noreply.github.com>
2022-11-07 13:24:35 +0100

    Début du projet

---
+++ exercice-ttt.js
@@ -0,0 +1,9 @@
+const etatJeu = {
+  cellules: [
+    [null, null, null],
+    [null, null, null],
+    [null, null, null],
+  ],
+  // Deux joueurs possibles : "BLEU" ou "ORANGE"
+  joueur: 'BLEU',
+};
